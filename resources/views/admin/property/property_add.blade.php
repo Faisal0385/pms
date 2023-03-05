bfc71a4d52f0e4e4b589e0efa15564c85a005eeb
Faisal A. Salam <faisaltez@gmail.com>
2023-03-05 22:48:35 +0600

    property add

--- resources/views/admin/property/property_add.blade.php
+++ resources/views/admin/property/property_add.blade.php
@@ -10,19 +10,13 @@
             <div class="card border-top border-0 border-4 border-primary">
                 <div class="card-body">
                     <form class="row g-3">
-                    <div class="col-md-6">
+                        <div class="col-md-6">
                             <label for="inputFirstName" class="form-label">
                                 <h5 class="mb-0 text-primary">Owner Name</h5>
                             </label>
-                            <input type="text" class="form-control" placeholder="Owner Name">
+                            <input type="text" class="form-control form-control-sm" placeholder="Owner Name">
                         </div>
                         <div class="col-md-6">
-                            <label for="inputFirstName" class="form-label">
-                                <h5 class="mb-0 text-primary">Property Name</h5>
-                            </label>
-                            <input type="text" class="form-control" placeholder="Property Name">
-                        </div>
-                        <!-- <div class="col-md-6">
                             <label for="inputFirstName" class="form-label">
                                 <h5 class="mb-0 text-primary">Building Name</h5>
                             </label>
@@ -34,34 +28,115 @@
                                     <option value="United States">KB Building</option>
                                 </select>
                             </div>
-                        </div> -->
+                        </div>
+
                         <div class="col-md-6">
                             <label for="inputFirstName" class="form-label">
-                                <h5 class="mb-0 text-primary">Property Category</h5>
+                                <h5 class="mb-0 text-primary">Unit Name</h5>
+                            </label>
+                            <input type="text" class="form-control form-control-sm" placeholder="Unit Name">
+                        </div>
+
+                        <div class="col-md-6">
+                            <label for="inputFirstName" class="form-label">
+                                <h5 class="mb-0 text-primary">Unit Type</h5>
                             </label>
                             <div class="mb-3 select2-sm ">
                                 <select class="single-select form-control form-control-sm">
-                                    <option value="United States">FLATS</option>
-                                    <option value="United States">OFFICES</option>
-                                    <option value="United States">SHOPS</option>
-                                    <option value="United States">WAREHOUSES</option>
-                                    <option value="United States">SHOW ROOM</option>
+                                    <option value="United States">Sheds</option>
+                                    <option value="United States">Shops</option>
+                                    <option value="United States">OFFICE</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputFirstName" class="form-label">
+                                <h5 class="mb-0 text-primary">Property Type</h5>
+                            </label>
+                            <div class="mb-3 select2-sm ">
+                                <select class="single-select form-control form-control-sm">
+                                    <option value="United States">G+1</option>
+                                    <option value="United States">G+5</option>
+                                    <option value="United States">G+4</option>
+                                    <option value="United States">G+6</option>
                                 </select>
                             </div>
                         </div>
 
                         <div class="col-md-6">
                             <label for="inputFirstName" class="form-label">
-                                <h5 class="mb-0 text-primary">Area sqft</h5>
+                                <h5 class="mb-0 text-primary">Plot</h5>
                             </label>
-                            <input type="text" class="form-control" placeholder="Area sqft">
+                            <input type="text" class="form-control form-control-sm" placeholder="Plot">
                         </div>
-                        <div class="col-md-12">
+                        <div class="col-md-6">
                             <label for="inputFirstName" class="form-label">
-                                <h6 class="mb-0 mt-2 text-primary">Building Pic</h6>
+                                <h5 class="mb-0 text-primary">Makani</h5>
+                            </label>
+                            <input type="text" class="form-control form-control-sm" placeholder="Makani">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputFirstName" class="form-label">
+                                <h5 class="mb-0 text-primary">Sector</h5>
+                            </label>
+                            <input type="text" class="form-control form-control-sm" placeholder="Sector">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputFirstName" class="form-label">
+                                <h5 class="mb-0 text-primary">Street</h5>
+                            </label>
+                            <input type="text" class="form-control form-control-sm" placeholder="Street">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="inputFirstName" class="form-label">
+                                <h5 class="mb-0 text-primary">District</h5>
+                            </label>
+                            <input type="text" class="form-control form-control-sm" placeholder="District">
+                        </div>
+
+                        <div class="col-md-6">
+                            <label for="inputFirstName" class="form-label">
+                                <h5 class="mb-0 text-primary">Purpose</h5>
+                            </label>
+                            <div class="mb-3 select2-sm ">
+                                <select class="single-select form-control form-control-sm">
+                                    <option value="United States">Rental</option>
+                                    <option value="United States">Commercial</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <label for="inputFirstName" class="form-label">
+                                <h5 class="mb-0 text-primary">Rental Amount</h5>
+                            </label>
+                            <input type="text" class="form-control form-control-sm" placeholder="Rental Amount">
+                        </div>
+
+
+
+
+
+
+
+                        <!-- <div class="col-md-12">
+                            <label for="inputFirstName" class="form-label">
+                                <h6 class="mb-0 mt-2 text-primary">Property Pic 1</h6>
                             </label>
                             <input type="file" class="form-control" id="inputGroupFile02">
                         </div>
+                        <div class="col-md-12">
+                            <label for="inputFirstName" class="form-label">
+                                <h6 class="mb-0 mt-2 text-primary">Property Pic 2</h6>
+                            </label>
+                            <input type="file" class="form-control" id="inputGroupFile02">
+                        </div>
+                        <div class="col-md-12">
+                            <label for="inputFirstName" class="form-label">
+                                <h6 class="mb-0 mt-2 text-primary">Property Pic 2</h6>
+                            </label>
+                            <input type="file" class="form-control" id="inputGroupFile02">
+                        </div> -->
                         <div class="col-lg-12">
                             <button type="submit" class="btn btn-sm btn-primary px-5">Submit</button>
                         </div>
